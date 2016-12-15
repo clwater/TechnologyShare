ec303d5d8c1be7b9c94db74403e3f8e79ae0f21d
gengzhibo <gengzhibo@gengzhibo.net>
2016-12-15 18:45:20 +0800

    20161215

--- xbb/source/RxJava/app/src/main/java/com/clwater/rxjava/MainActivity.java
+++ xbb/source/RxJava/app/src/main/java/com/clwater/rxjava/MainActivity.java
@@ -2,6 +2,17 @@ package com.clwater.rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(666);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread())
+           .observeOn(AndroidSchedulers.mainThread())
+           .subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("gzb" , "onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d("gzb" , "onNext: " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("gzb" , "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("gzb" , "onComplete");
+            }
+        });
+        
     }
 }
