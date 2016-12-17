4e23e2886301f04dd97ff0297b68ba95da527e2f
gengzhibo <gengzhibo@gengzhibo.net>
2016-12-17 19:08:48 +0800

    20161217

--- xbb/source/RxJava/app/src/main/java/com/clwater/rxjava/MainActivity.java
+++ xbb/source/RxJava/app/src/main/java/com/clwater/rxjava/MainActivity.java
@@ -32,14 +32,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Consumer<Integer> consumer = new Consumer<Integer>() {
-            @Override
-            public void accept(Integer integer) throws Exception {
-                Log.d("gzb", "Observer thread is :" + Thread.currentThread().getName());
-                Log.d("gzb", "onNext: " + integer);
-            }
-        };
-
+//        Consumer<Integer> consumer = new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) throws Exception {
+//                Log.d("gzb", "Observer thread is :" + Thread.currentThread().getName());
+//                Log.d("gzb", "onNext: " + integer);
+//            }
+//        };
         observable.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(consumer);
